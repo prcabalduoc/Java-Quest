2a7fe28b31012b9f2a7c9c9c92a075c25039e46c
Pablo Ramirez <pabl.ramirezc@duocuc.cl>
2026-09-03 21:42:28 -0400

    JAVA-010 - Agregar procesamiento con for

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Procesando estudiante " + i);
+        }
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingrese nombre: ");
