6cb35717eb5dce8e16f6874417180b2a3a48bd86
Pablo Ramirez <pabl.ramirezc@duocuc.cl>
2026-09-03 21:29:27 -0400

    JAVA-006 - Implementar Main y crear Estudiante

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
-void main() {
-    //
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiante estudiante = new Estudiante(
+                "Ana",
+                "Ingeniería en Informática",
+                20
+        );
+
+        estudiante.mostrarInformacion();
+    }
 }
